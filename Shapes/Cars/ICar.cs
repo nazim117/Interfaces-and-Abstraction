4ead1e6357d82255873664be4107db0bf0cf4b60
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-12-05 16:37:08 +0100

    Uploaded project

---
+++ Shapes/Cars/ICar.cs
@@ -0,0 +1,10 @@
+﻿namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; protected set; }
+        public string Color { get; protected set; }
+        public string Start();
+        public string Stop();
+    }
+}
---
+++ Shapes/Shapes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿/*
+3
+4
+5
+ 
+*/
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+            circle.Draw();
+            rect.Draw();
+
+        }
+    }
+}
